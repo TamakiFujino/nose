0069ffc7aa43f646f22fc105b1984deb07d4de03
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-01-19 14:43:02 +0900

    initial commit

---
+++ noseTests/noseTests.swift
@@ -0,0 +1,17 @@
+//
+//  noseTests.swift
+//  noseTests
+//
+//  Created by Tamaki Fujino on 2025/01/18.
+//
+
+import Testing
+@testable import nose
+
+struct noseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
