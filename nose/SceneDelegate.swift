bf8486185cf9b41465ed6ce760fe3fceb7e27612
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-05-20 23:58:37 +0900

    search function

--- nose/SceneDelegate.swift
+++ nose/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = SplashViewController()
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 
