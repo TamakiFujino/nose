06367d0bde030d4d8efde4efa4eb62acd7ee83a3
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-06-08 18:13:35 +0900

    test added

--- AppiumTests/tests/base_test.py
+++ AppiumTests/tests/base_test.py
@@ -3,17 +3,22 @@ from utils.driver_setup import DriverSetup
 
 class BaseTest(unittest.TestCase):
     driver = None
+    _test_count = 0
+    _test_completed = 0
 
     @classmethod
     def setUpClass(cls):
         if BaseTest.driver is None:
             BaseTest.driver = DriverSetup().get_driver()
+            # Count total number of test classes
+            BaseTest._test_count = len([name for name in dir(cls) if name.startswith('test_')])
         cls.driver = BaseTest.driver
 
     @classmethod
     def tearDownClass(cls):
-        # Only quit the driver after all tests are complete
-        if cls.__name__ == 'HomeCurrentTest':  # Last test class
+        BaseTest._test_completed += 1
+        # Only quit the driver after all test classes are complete
+        if BaseTest._test_completed >= BaseTest._test_count:
             if BaseTest.driver is not None:
                 BaseTest.driver.quit()
                 BaseTest.driver = None 
