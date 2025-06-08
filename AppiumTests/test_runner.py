035eb20f785ab76a5861da5ec93cd7a2d6c78f27
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-06-08 18:13:35 +0900

    test added

--- AppiumTests/test_runner.py
+++ AppiumTests/test_runner.py
@@ -1,7 +1,15 @@
 import unittest
+import sys
+import os
 
 if __name__ == "__main__":
+    # Get all test files in the tests directory
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('tests')
+    
+    # Run the tests
     test_runner = unittest.TextTestRunner()
-    test_runner.run(test_suite)
+    result = test_runner.run(test_suite)
+    
+    # Exit with appropriate status code
+    sys.exit(not result.wasSuccessful())
---
+++ AppiumTests/tests/tests_03_home_future.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import time
+import unittest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from tests.base_test import BaseTest
+
+class HomeFutureTest(BaseTest):
+    def test_home_future(self):
+        # find a element with elementid 27000000-0000-0000-CBAC-000000000000
+        # which is the future dot
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'right_dot')
+        element.click()
+        time.sleep(2)
+        # find a button with accessibility id "sparkle"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'sparkle')
+        element.click()
+        time.sleep(2)
+        # make sure the modal title is "My Collections"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'My Collections')
+        assert element.text == 'My Collections'
+        # tap "National Parks" collection
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'National Parks')
+        element.click()
+        time.sleep(2)
+        # check the saved spot is listed
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Kings Canyon National Park')
+        assert element.text == 'Kings Canyon National Park'
+        # check the number of spots is 1
+        # element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, '2')
+        # assert element.text == '1 spot'
+        # tap three dot button
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More')
+        element.click()
+        time.sleep(2)
+        # tap "Share with Friends"
+        # element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Share with Friends')
+        # element.click()
+        # time.sleep(2)
+        # tap "Share"
+        # element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Share')
+        # element.click()
+        # tap "close"
+        # element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'close')
+        # element.click()
+        
+        # complete the collection
+        # tap "Complete"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Complete the Collection')
+        element.click()
+        time.sleep(2)
+        # tap "Complete"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Complete')
+        element.click()
+        time.sleep(5)
+        # tap the center of the screen to close the modal
+        self.driver.tap([(500, 500)])
+        time.sleep(2)
+
+if __name__ == '__main__':
+    unittest.main()
