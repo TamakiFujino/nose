bdc3c672da87015adbda21944f9407e72546cd8d
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-06-10 11:07:13 +0900

    appium test update

--- AppiumTests/tests/tests_0202_home_current.py
+++
@@ -1,9 +0,0 @@
-import time
-from appium.webdriver.common.appiumby import AppiumBy
-
-class TestHomeCurrent:
-    def test_home_current(self):
-        # click the first suggestion from the search result, not mentioning the name
-        element = self.driver.find_element(AppiumBy.ID, '27000000-0000-0000-CBAC-000000000000')
-        element.click()
-        time.sleep(2) 
