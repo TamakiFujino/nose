d1a68b6d1474337a2d9d6d6b5db596620b572a3f
Tamaki Fujino <tamaki.fujino@sjsu.edu>
2025-06-10 11:57:54 +0900

    collection creation

---
+++ AppiumTests/tests/tests_02_CollectionCreation_01.py
@@ -0,0 +1,134 @@
+import sys
+import os
+import time
+import unittest
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from tests.base_test import BaseTest
+from tests import google_login, logout
+from utils import config
+
+class CollectionCreationTest(BaseTest):
+    def test_create_collection(self):
+        """login"""
+        google_login(self.driver, 'user_a')
+
+        """search a spot"""
+        # click the search button icon
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Search')
+        element.click()
+        time.sleep(2)
+
+        # type "Kings Canyon national park" in the sarch bar
+        search_bar = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Search for a place')
+        search_bar.click()
+        search_bar.send_keys('Ki')
+        time.sleep(5)
+        search_bar.send_keys('ngs ')
+        time.sleep(3)
+        search_bar.send_keys('Canyon')
+        time.sleep(3)
+
+        # click the first suggestion from the search result, not mentioning the name
+        element = self.driver.find_element(By.XPATH, '//XCUIElementTypeCell[1]')
+        element.click()
+        time.sleep(2)
+
+        # check the title of the space
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Kings Canyon National Park')
+        # click the save button with accessibiliy id "bookmark"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'bookmark')
+        element.click()
+        time.sleep(2)
+
+        # check the title of the modal
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Save to Collection')
+
+        """create a collection"""
+        # click add button
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'add')
+        element.click()
+        time.sleep(2)
+        # find "XCUIElementTypeTextField", and type "National Parks" in the text field
+        text_field = self.driver.find_element(By.XPATH, '//XCUIElementTypeTextField')
+        text_field.click()
+        text_field.send_keys('National Parks')
+        # tap Create
+        create_button = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Create')
+        create_button.click()
+        time.sleep(2)
+
+        """add a spot to the collection"""
+        # click save button
+        save_button = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Save')
+        save_button.click()
+        time.sleep(2)
+
+        # tap the center of the screen to close the modal
+        self.driver.tap([(500, 500)])
+        time.sleep(2)
+
+        """check the collection content"""
+        # which is the future dot
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'right_dot')
+        element.click()
+        time.sleep(2)
+        # find a button with accessibility id "sparkle"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'sparkle')
+        element.click()
+        time.sleep(2)
+        # make sure the modal title is "My Collections"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'My Collections')
+        assert element.text == 'My Collections'
+        # tap "National Parks" collection
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'National Parks')
+        element.click()
+        time.sleep(2)
+        # check the saved spot is listed
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Kings Canyon National Park')
+        assert element.text == 'Kings Canyon National Park'
+        
+        # check the number of spots is 1
+
+        """share the collection with user B"""
+        # tap three dot button
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More')
+        element.click()
+        time.sleep(2)
+        # tap "Share with Friends"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Share with Friends')
+        element.click()
+        time.sleep(2)
+        # select User B
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, config.TEST_USERS["user_b"]["updated_name"])
+        element.click()
+        time.sleep(2)
+        # tap "Share"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Update Sharing')
+        element.click()
+        time.sleep(2)
+        
+        """complete the collection"""
+        # tap three dot button
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More')
+        element.click()
+        time.sleep(2)
+        # tap "Complete the Collection"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Complete the Collection')
+        element.click()
+        time.sleep(2)
+        # tap "Complete"
+        element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Complete')
+        element.click()
+        time.sleep(5)
+        # tap somewhere on the screen to close the modal
+        self.driver.tap([(200, 200)])
+        time.sleep(2)
+
+        """log out"""
+        logout(self.driver)
+
+if __name__ == '__main__':
+    unittest.main()
